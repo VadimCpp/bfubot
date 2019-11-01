2f989cafac2066027f19e08c998aeb00f9063e74
navacar <55153165+navacar@users.noreply.github.com>
2019-11-01 11:11:42 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,8 @@ import json
 from config import *
 from firebase import firebase
 
-
+# Login: bfubot123@gmail.com
+# Password: Firdu8-jantih-tanxon
 firebase = firebase.FirebaseApplication("https://bfubot.firebaseio.com/", None)
 # NOTE 
 # old jsonString
