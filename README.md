f323f6518844b94ea3ab3a09c471f640b8af89b2
Wag-ON <51058872+Wag-ON@users.noreply.github.com>
2019-12-24 21:10:21 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# bfubot
+# pyfirstbotbot
 
-> Telegram bot: [https://t.me/pyfirstbotbot](https://t.me/bfu)
+> Telegram bot: [https://t.me/pyfirstbotbot](https://t.me/pyfirstbotbot)
 
 This is the starter template for other bots built with python
 
@@ -9,15 +9,18 @@ This is the starter template for other bots built with python
 Пошаговая инструкция находится:
 [frontend-basics.blogspot.com/2019/09/py-first-bot.html](https://frontend-basics.blogspot.com/2019/09/py-first-bot.html)
 
-
-Инструкция к FireBase:
-1) Заходим на сайт https://google.com
-2) Входим в аккаунт разработки (Логин и пороль в файле bot.py)
-3) Делаем аккаунт разработки, аккаунтом по умолчанию
-4) Заходим на сайт https://console.firebase.google.com/u/0/project/bfubot/database/bfubot/data
-5) Работаем с датабазой.
-
 ## Настройка IDE
 
-**TODO:**
+[Ссылка на скачивание PyCharm.][pycharm_download]
+Можно использовать как Community, так и триал Professional.
+Professional можно получить на год для учебных целей.
+Если не уложитесь в 30 дней, есть возможность сбрасывать триал.
 
+
+Для форматирования кода использовать комбинацию Ctrl+Alt+l (l == L). 
+Не требует настройки.
+Использовать для каждого файла по отдельности.
+**Перед коммитом/пушем форматируйте код.**
+
+[//]: # (These are reference links used in the body of this note and get stripped out when the markdown processor does its job. There is no need to format nicely because it shouldn't be seen. Thanks SO - http://stackoverflow.com/questions/4823468/store-comments-in-markdown-syntax)
+[pycharm_download]:<https://www.jetbrains.com/pycharm/download/>
--- bot.py
+++ bot.py
@@ -4,15 +4,23 @@ import json
 from config import *
 from firebase import firebase
 
+from flask import Flask, request
+
+MODE = Config.MODE
+TOKEN = Config.BOT_TOKEN
+URL = Config.URL
+
+
 # FireBase Account:
 # Login: bfubot123@gmail.com
 # Password: Firdu8-jantih-tanxon
-firebase = firebase.FirebaseApplication("https://bfubot.firebaseio.com/", None)
-# NOTE 
+
+# NOTE
 # old jsonString
 #jsonString = '{ "Group": "MO-2", "TimeTable":{"Day":[ {"DayName":"Вторник", "time": "15-20","Cab": "(ауд. 231)", "Lesson": "лекция Python"}, {"DayName":"Среда", "Подгруппа": [{"Номер":"(2я подгруппа)", "time":"10-10", "Cab":"(ауд. 235)",  "Lesson": "лаба Python"}, {"Номер":"(3я подгруппа)","time":"12-00", "Cab": "(ауд. 235)", "Lesson":"лаба Python"}, {"Номер":"(1я подгруппа)", "time":"13-40", "Cab":"(ауд. 230)", "Lesson": "лаба Python"} ]} ]} }'
-jsonString_pm4='{ "Group": "ПМ-4", "TimeTable":{"Day":[  {"DayName":"Пятница", "Подгруппа":[{"Номер":"(1я подгруппа)", "time":"10-10", "Cab":"(ауд. 230А. Вход через 230 ауд.)", "Lesson": "лаба Python"},{"time":"12-00", "Cab":"(ауд.118)", "Lesson": "лекция Python"},{"Номер":"(2я подгруппа)", "time":"13-40", "Cab":"(ауд. 214)", "Lesson": "лаба Python"} ]} ]} }'
 
+firebase = firebase.FirebaseApplication("https://bfubot.firebaseio.com/", None)
+jsonString_pm4='{ "Group": "ПМ-4", "TimeTable":{"Day":[  {"DayName":"Пятница", "Подгруппа":[{"Номер":"(1я подгруппа)", "time":"10-10", "Cab":"(ауд. 230А. Вход через 230 ауд.)", "Lesson": "лаба Python"},{"time":"12-00", "Cab":"(ауд.118)", "Lesson": "лекция Python"},{"Номер":"(2я подгруппа)", "time":"13-40", "Cab":"(ауд. 214)", "Lesson": "лаба Python"} ]} ]} }'
 result = firebase.get('/bfubot/Schedule', '')
 result = str(result)
 count = 0
@@ -31,25 +39,45 @@ jsonString = result
 obj = json.loads(jsonString)
 obj_pm4 =json.loads(jsonString_pm4)
 
+
+
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')  # Ну это по классике, логи
+logger = logging.getLogger()
 
 bot = telebot.TeleBot(Config.BOT_TOKEN)  # Создает объект класса "TeleBot", то есть нашего бота
+def init_server():
+	server = Flask(__name__)
+
+	# Инициализирует webhook
+	@server.route("/")
+	def webhook():
+		bot.remove_webhook()
+		bot.set_webhook(url=URL + TOKEN)
+		return "!", 200
+
+	# Обработывает события
+	@server.route('/' + TOKEN, methods=['POST'])
+	def getMessage():
+		bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+		return "!", 200
+
+	return server
 
 def printSchedule(bot, message, groupNo):
-    
-    output = "Группа " + obj["Group"] + "\n\n" 
-    
+
+    output = "Группа " + obj["Group"] + "\n\n"
+
     if groupNo == "/mo2":
-        
-        i = 0    
+
+        i = 0
         day = obj["TimeTable"]['Day']
         for i in range(2):
             if i == 0:
                 output += "*" + day[i]["DayName"] + "*\n" + day[i]['time'] + ' ' + day[i]['Cab'] + ' ' + day[i]['Lesson'] + '\n\n'
             if i == 1:
                 output += '*' + day[i]["DayName"] + '*\n'
-    
+
         i = 0
 
         for i in range(3):
@@ -75,10 +103,10 @@ def printSchedule(bot, message, groupNo):
                          obj_pm4['TimeTable']["Day"][0]["Подгруппа"][2]["Cab"] + " " +
                          obj_pm4['TimeTable']["Day"][0]["Подгруппа"][2]['Lesson'] + " " +
                          obj_pm4['TimeTable']["Day"][0]["Подгруппа"][2]["Номер"] + '\n', parse_mode="Markdown")
-    
+
     elif message.chat.id > 0:
         bot.send_message(chat_id=message.chat.id, text=autosending_text(bot, message), parse_mode='html',
-                         disable_web_page_preview=True)  # Отправляет авто сообщение 
+                         disable_web_page_preview=True)  # Отправляет авто сообщение
 
 
 @bot.message_handler(
@@ -91,10 +119,10 @@ def printSchedule(bot, message, groupNo):
 @bot.message_handler(commands=['mo2'])
 
 def sending_auto2(message):
-    
+
     printSchedule(bot, message, message.text)
-    
-    
+    #bot.send_message(chat_id=message.chat.id, text=autosending_text(bot, message), parse_mode='html',disable_web_page_preview=True)  # Отправляет авто сообщение
+
     # NOTE
     # Не отправляем сообщения в общие чаты.
     # else:
@@ -102,4 +130,11 @@ def sending_auto2(message):
 #sending_auto2(message)
 
 if __name__ == '__main__':
-    bot.polling()  # Заставляет бота получать уведомления о новых сообщениях
+    logging.info("Selected mode " + MODE)
+    if MODE == "debug":
+        bot.remove_webhook()
+        bot.polling() # Заставляет бота получать уведомления о новых сообщениях
+    else:
+		# Сервер необходим для работы webhook
+        server = init_server()
+        server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
